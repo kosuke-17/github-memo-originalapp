52d5de1ca48ad510aba986ee3f245f81f21c75cc
kosuke-17 <tamutamu17@icloud.com>
2021-12-26 11:24:35 +0900

    レイアウト作成

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,7 +1,14 @@
 import type { AppProps } from "next/app";
+import Layout from "../components/Layout";
 import "../styles/tailwind.css";
+import "antd/dist/antd.css";
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp;

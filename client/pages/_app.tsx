010d455f61a743fd15934c2b924fd2b75d7e088b
kosuke-17 <tamutamu17@icloud.com>
2021-12-31 20:32:07 +0900

    不要なコードをコメントアウトアウト

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,34 +2,34 @@ import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
 import "../styles/tailwind.css";
 import "antd/dist/antd.css";
-import {
-  ApolloClient,
-  InMemoryCache,
-  createHttpLink,
-  ApolloProvider,
-} from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
+// import {
+//   ApolloClient,
+//   InMemoryCache,
+//   createHttpLink,
+//   ApolloProvider,
+// } from "@apollo/client";
+// import { setContext } from "@apollo/client/link/context";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const httpLink = createHttpLink({ uri: process.env.GRAPHQL_ENDPOINT });
-  const authLink = setContext((_, { headers }) => {
-    return {
-      headers: {
-        ...headers,
-        authorization: `Bearer ${process.env.GITHUB_ACCESS_TOKEN}`,
-      },
-    };
-  });
-  const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache(),
-  });
+  // const httpLink = createHttpLink({ uri: process.env.GRAPHQL_ENDPOINT });
+  // const authLink = setContext((_, { headers }) => {
+  //   return {
+  //     headers: {
+  //       ...headers,
+  //       authorization: `Bearer ${process.env.GITHUB_ACCESS_TOKEN}`,
+  //     },
+  //   };
+  // });
+  // const client = new ApolloClient({
+  //   link: authLink.concat(httpLink),
+  //   cache: new InMemoryCache(),
+  // });
   return (
-    <ApolloProvider client={client}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ApolloProvider>
+    // <ApolloProvider client={client}>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+    // </ApolloProvider>
   );
 }
 

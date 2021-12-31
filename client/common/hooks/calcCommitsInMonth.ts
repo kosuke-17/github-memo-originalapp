5621466bb310b9bc9c725d59157b74267329edc8
kosuke-17 <tamutamu17@icloud.com>
2021-12-31 20:35:25 +0900

    コミット計算の処理を分離

---
+++ client/common/hooks/calcCommitsInMonth.ts
@@ -0,0 +1,43 @@
+import { CONTRIBUTIONCALENDARWEEKS, CONTRIBUTIONDAY } from "../Types";
+
+export const totalCommitsInMonth = ({
+  contributionCalendarWeeks,
+}: CONTRIBUTIONCALENDARWEEKS): number[] => {
+  let monthCommitsInYear = new Array<number[]>();
+  let totalCommitsInMonth = new Array<number>();
+
+  // 各月ごとにコミット数を分けている
+  for (const contributionCalendarWeek of contributionCalendarWeeks) {
+    monthCommitsInYear.push(new Array<number>());
+
+    contributionCalendarWeek.map((day: CONTRIBUTIONDAY) => {
+      // 1月から10月までの日にちごとのコミット数をプッシュ
+      for (let i = 0; i <= 9; i++) {
+        if (day.date.includes(`2021-0${i + 1}`)) {
+          monthCommitsInYear[i].push(day.contributionCount);
+        }
+      }
+      // 11月と12月の日にちごとのコミット数をプッシュ
+      for (let i = 10; i < 12; i++) {
+        if (day.date.includes(`2021-${i + 1}`)) {
+          monthCommitsInYear[i].push(day.contributionCount);
+        }
+      }
+    });
+  }
+
+  // 各月ごとのコミット総数を算出している
+  for (let i = 0; i < 12; i++) {
+    let sum = monthCommitsInYear[i].reduce(function (
+      previousValue: number,
+      currentValue: number
+    ) {
+      return previousValue + currentValue;
+    },
+    0);
+    totalCommitsInMonth.push(sum);
+  }
+
+  // 各月のコミット数を返す
+  return totalCommitsInMonth;
+};
--- client/components/templates/LineGraph.tsx
+++ client/components/templates/LineGraph.tsx
@@ -1,45 +1,16 @@
 import React from "react";
 import { Line } from "react-chartjs-2";
+import { totalCommitsInMonth } from "../../common/hooks/calcCommitsInMonth";
 import { MONTH_LABEL } from "../../common/MONTH_LABEL";
-import { CONTRIBUTIONDAY, CONTRIBUTIONCALENDARWEEKS } from "../../common/Types";
+import { CONTRIBUTIONCALENDARWEEKS } from "../../common/Types";
 
 const LineGraph: React.FC<CONTRIBUTIONCALENDARWEEKS> = ({
   contributionCalendarWeeks,
 }) => {
-  let monthCommitsInYear = new Array<number[]>();
-  let totalCommitsInMonth = new Array<number>();
-
-  // 各月ごとにコミット数を分けている
-  for (const contributionCalendarWeek of contributionCalendarWeeks) {
-    monthCommitsInYear.push(new Array<number>());
-
-    contributionCalendarWeek.map((day: CONTRIBUTIONDAY) => {
-      // 1月から10月までの日にちごとのコミット数をプッシュ
-      for (let i = 0; i <= 9; i++) {
-        if (day.date.includes(`2021-0${i + 1}`)) {
-          monthCommitsInYear[i].push(day.contributionCount);
-        }
-      }
-      // 11月と12月の日にちごとのコミット数をプッシュ
-      for (let i = 10; i < 12; i++) {
-        if (day.date.includes(`2021-${i + 1}`)) {
-          monthCommitsInYear[i].push(day.contributionCount);
-        }
-      }
-    });
-  }
-
-  // 各月ごとのコミット総数を算出している
-  for (let i = 0; i < 12; i++) {
-    let sum = monthCommitsInYear[i].reduce(function (
-      previousValue: number,
-      currentValue: number
-    ) {
-      return previousValue + currentValue;
-    },
-    0);
-    totalCommitsInMonth.push(sum);
-  }
+  //各月のコミット数を取得
+  const commitsDataInMonth: number[] = totalCommitsInMonth({
+    contributionCalendarWeeks,
+  });
 
   // ライングラフ用のデータを用意
   const data = {
@@ -48,7 +19,7 @@ const LineGraph: React.FC<CONTRIBUTIONCALENDARWEEKS> = ({
       {
         label: "Commit",
         fill: false,
-        data: totalCommitsInMonth,
+        data: commitsDataInMonth,
         borderColor: "rgba(75,192,192,1)",
         pointBorderWidth: 5,
       },

7d3515fe967a85ac8575ef7da0852e9fb5f46152
kosuke-17 <tamutamu17@icloud.com>
2022-01-04 07:15:07 +0900

    選択した月のコミット数を表示

--- client/pages/linegraph-page.tsx
+++ client/pages/linegraph-page.tsx
@@ -16,9 +16,17 @@ const { Option } = Select;
 const LineGraphPage: React.FC<CONTRIBUTIONCALENDARWEEKS> = ({
   contributionCalendarWeeks,
 }) => {
+  // 年、月グラフ用のフラグ
   const [graphCmponent, setGraphCmponent] = useState<string>("year");
+  // 各月グラフ用のフラグ
+  const [currentMonth, setCurrentMonth] = useState<string>("12");
+  // 年か月のグラフを切り替えるためのflag変更処理
   const chngeGraph = (YearOrMonth: string) => {
     setGraphCmponent(YearOrMonth);
+  };
+  // 各月のグラフを切り替えるためのflag変更処理
+  const chngeMonth = (selectedMonth: string) => {
+    setCurrentMonth(selectedMonth);
   };
 
   return (
@@ -43,9 +51,35 @@ const LineGraphPage: React.FC<CONTRIBUTIONCALENDARWEEKS> = ({
             contributionCalendarWeeks={contributionCalendarWeeks}
           />
         ) : (
-          <LineGraphInDays
-            contributionCalendarWeeks={contributionCalendarWeeks}
-          />
+          <span>
+            <Select
+              placeholder="年"
+              defaultValue={currentMonth}
+              style={{
+                width: 120,
+                paddingLeft: 20,
+                marginTop: 20,
+              }}
+              onChange={chngeMonth}
+            >
+              <Option value="1">1月</Option>
+              <Option value="2">2月</Option>
+              <Option value="3">3月</Option>
+              <Option value="4">4月</Option>
+              <Option value="5">5月</Option>
+              <Option value="6">6月</Option>
+              <Option value="7">7月</Option>
+              <Option value="8">8月</Option>
+              <Option value="9">9月</Option>
+              <Option value="10">10月</Option>
+              <Option value="11">11月</Option>
+              <Option value="12">12月</Option>
+            </Select>
+            <LineGraphInDays
+              contributionCalendarWeeks={contributionCalendarWeeks}
+              currentMonth={currentMonth}
+            />
+          </span>
         )}
       </GraphCard>
     </>
